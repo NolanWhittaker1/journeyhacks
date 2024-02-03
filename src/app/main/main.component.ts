095c02f9e0123b0f8a7203bb69b82b6b4f05736f
Nolan Whittaker <nolan_whittaker@sfu.ca>
2024-02-02 17:53:52 -0800

    6pm work

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,19 +9,43 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class MainComponent  {
   countdown: string;
   startVisible: boolean = true;
-  currentEvent: string;
+  timeLength: any;
+  minute: number;
   constructor() {
-    this.countdown = "05:00"; // Initial value set to 5 minutes
-    this.currentEvent = "timer";
+    this.countdown = ""; 
+    const timeLengthStorage = localStorage.getItem("timeRemaining");
+    this.timeLength = timeLengthStorage  ? JSON.parse(timeLengthStorage) : 5;
+    this.minute = 5;
   }
 
-  onStart() {
+  onStart() { // This is ran when the start button is clicked
+    let seconds: number = this.minute * 60;
+    let textSec: any = "0";
+    let statSec: number = 60;
+
+    const prefix = this.minute < 10 ? "0" : "";
+
+    const timer = setInterval(() => {
+      seconds--;
+      if (statSec != 0) statSec--;
+      else statSec = 59;
+
+      if (statSec < 10) {
+        textSec = "0" + statSec;
+      } else textSec = statSec;
+
+      this.countdown = `${prefix}${Math.floor(seconds / 60)}:${textSec}`;
+
+      if (seconds == 0) {
+        console.log("finished");
+        clearInterval(timer);
+      }
+    }, 1000);
   }
 
   toggleDisplays(): void {
     this.startVisible = !this.startVisible;
   }
-
-  
+ 
 }
 
