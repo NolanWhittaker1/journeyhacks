fc81612f3b573e94402e0f21db502a2063a1eb35
Nolan Whittaker <nolan_whittaker@sfu.ca>
2024-02-02 17:15:50 -0800

    Allat

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+export class MainComponent  {
+  countdown: string;
+  startVisible: boolean = true;
+  currentEvent: string;
+  constructor() {
+    this.countdown = "05:00"; // Initial value set to 5 minutes
+    this.currentEvent = "timer";
   }
 
+  onStart() {
+  }
+
+  toggleDisplays(): void {
+    this.startVisible = !this.startVisible;
+  }
+
+  
 }
+
